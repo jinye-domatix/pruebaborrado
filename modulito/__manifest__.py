de083e03698706e8ecd5d8286c13a9019386d577
jinye <jinye@domatix.com>
2024-07-07 21:27:47 +0200

    fasdf

---
+++ modulito/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'odoo2android - customer sale',
+    'version': '13.0.0.0.1',
+    'summary': '',
+    'description': '',
+    'author': 'Domatix',
+    'website': 'domatix.com',
+    'depends': [
+    ],
+    'data': [],
+    'installable': True,
+}
